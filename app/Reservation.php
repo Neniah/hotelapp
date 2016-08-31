dccc7658bca11706098c8b90053e455657e24e68
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-08-31 19:28:19 +0200

    protected, fillable, others...

--- app/Reservation.php
+++ app/Reservation.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     //
+    protected $fillable = ['total_price', 'occupancy', 'checkin', 'checkout', 'name'];
+
+    public function nights(){
+      return $this->hasMany('App\ReservationNight');
+    }
+
+    public Customer(){
+      return $this->belongsTo('App\Customer');
+    }
 }
--- app/RoomCalendar.php
+++ app/RoomCalendar.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class RoomCalendar extends Model
 {
     //
+    protected $fillable = ['room_type_id', 'rate', 'day'];
+
+    function RoomType(){
+      return $this->hasOne('App\RoomType');
+    }
 }
