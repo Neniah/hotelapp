9d556ee2375416f826e6ad26fdb48d8dc41f6e69
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-01 15:40:51 +0200

    room calendar controller

---
+++ app/Http/Controllers/RoomCalendarController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class RoomCalendarController extends Controller
+{
+    //
+    public function setPriceInRangeForRoomType(Request $request){
+      
+    }
+}
